5ec08a26d9e99f262e5bd86c2adea1ff3dc8e8e3
Ievgenii Makhonko <yobaua@gmail.com>
2021-03-28 18:37:51 +0800

    Commit

---
+++ FizzBuzzV2/FizzBuzzV2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzzV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number lolka: ");
+            NewMethod(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        private static int NewMethod(int a)
+        {
+            if (a % 15 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (a % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (a % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            return a;
+        }
+    }
+}
